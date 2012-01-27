7e951ed69316cae5369f07cac1eb13419cef0d89
Davis Balgley <davis.balgley@gmail.com>
2012-01-27 14:43:38 -0700

    Level 4 completed, changed up structure greatly, using def instead of loops all the time

--- project02/rubywarrior/null-pointer-beginner/player.rb
+++ project02/rubywarrior/null-pointer-beginner/player.rb
@@ -1,23 +1,27 @@
 class Player
-  def play_turn(warrior)
-    if warrior.health <= 17 && warrior.feel.empty?
-        warrior.rest!
-    else if @health => warrior.health
-        if warrior.feel.empty?
-            warrior.walk!
-        else
-            warrior.attack!
-    else if warrior.feel.empty?
-            warrior.walk!
-    else
-            warrior.attack!
-  end
-      @health = warrior.health
-end
-end
-end
-end
-end
-end
+	def initialize
+		@health = 0
+	end
+	def safe?
+		@warrior.feel.empty?
+	end
+	def injured?
+		@warrior.health < 12
+	end
+	def safeact
+		if (injured? and @warrior.health >= @health)
+			@warrior.rest!
+		else
+			@warrior.walk!
+		end
+	end
+  	def play_turn(warrior)
+	@warrior = warrior
+	if (safe?)
+		safeact
+	else
+		warrior.attack!
+	end
+	@health = warrior.health
 end
 end
