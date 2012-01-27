a1b81d6adf9d8aa3ac95d356a0a7456f7aa6bc85
Davis Balgley <davis.balgley@gmail.com>
2012-01-27 00:52:22 -0700

    Adding example 1 from learn ruby the hard way for project02.  Haw-yee?

---
+++ project02/ex1.rb
@@ -0,0 +1,11 @@
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts 'yay! Printing.'
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
+puts "Another Line"
+puts "?"
+puts "That's not what that's called.  That's called a gorram number symbol"
+#puts "Lies"
