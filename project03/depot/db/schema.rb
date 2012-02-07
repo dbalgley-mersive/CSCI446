d3d527f4250df863a139b437b3229e5d61bb6460
Davis Balgley <davis.balgley@gmail.com>
2012-02-06 23:01:38 -0700

    Cart

--- project03/depot/db/schema.rb
+++ project03/depot/db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120206191224) do
+ActiveRecord::Schema.define(:version => 20120207055843) do
+
+  create_table "carts", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "products", :force => true do |t|
     t.string   "title"
