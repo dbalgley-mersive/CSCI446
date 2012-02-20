4432c2ad34fe2c8c75d1fcb3b07ad3071d8c2299
Davis Balgley <davis.balgley@gmail.com>
2012-02-19 17:43:12 -0700

    Project 4, started, basic outline down, working on scrolling datapane

---
+++ project04/db/migrate/20120219224350_create_articles.rb
@@ -0,0 +1,11 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :author_name
+      t.text :body
+      t.date :creation_date
+      t.timestamps
+    end
+  end
+end
---
+++ project04/db/seeds.rb
@@ -0,0 +1,155 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+Article.delete_all
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+	Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+	Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
+
+Article.create(title: 'Lorem Ipsum',
+	author_name: 'Robert Frost',
+	body: 
+	%{<p>Robert Frost was born in San Fransisco in 19XX...
+		</p>},
+	creation_date: 'Tuesday, Dec. 6 2011')
