bbbdd429195a1875df67e35c7c362ca66e495a15
Davis Balgley <davis.balgley@gmail.com>
2012-04-08 14:39:54 -0600

    ReCaptcha added

--- project06/gamez/config/environment.rb
+++ project06/gamez/config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Gamez::Application.initialize!
+
+ENV['RECAPTCHA_PUBLIC_KEY']  = '6Lek-c8SAAAAAG_f8F0D4YTNbi2oi9bas9vwoYN_'
+ENV['RECAPTCHA_PRIVATE_KEY'] = '6Lek-c8SAAAAAHZTLiLTEKN2NnsfsfcANRI0Lx2i'
